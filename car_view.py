9d8199e1500a3ec423651a53405440e1c5317a03
ruipires <ruipedro.pirees999@gmail.com>
2025-06-04 15:55:06 +0100

    fix: correct format for Zenoh connection key in subscriber configuration

--- car_view.py
+++ car_view.py
@@ -113,7 +113,7 @@ def main():
     config = zenoh.Config()
     
     # Connect to the publisher's IP address
-    config.insert_json5(zenoh.config.CONNECT_KEY, "tcp/100.117.122.95:7447")
+    config.insert_json5(zenoh.config.CONNECT_KEY, '["tcp/100.117.122.95:7447"]')
     
     config.insert_json5("scouting/multicast/enabled", "true")
 
